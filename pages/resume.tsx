bf7215847bc06d83e0645e64150615db96bccbd8
parsasi <parsasi@rocketmail.com>
2020-12-07 10:52:21 +0330

    Resume Projects section created

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -3,8 +3,8 @@ import Layout from '../components/Layout'
 import PostList from '../components/PostList'
 import styleConstants from '../styles/constants'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faPhone , faAt } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import { faLink } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export default function Contact({ title, description, ...props }){
   return (
@@ -49,12 +49,14 @@ export default function Contact({ title, description, ...props }){
           display:flex;
           justify-content:space-between;
           margin:30px 0;
+          flex-wrap:wrap;
         }
 
         .projects>.resume-item{
-          width:calc(50% - 20px);
+          width:calc(50% - 10px);
           padding:20px 10px;
           background-color:${styleConstants.color.secondary[2]};
+          margin:10px 0;
         }
 
         .resume-item-top{
@@ -63,6 +65,33 @@ export default function Contact({ title, description, ...props }){
           align-items:center;
         }
 
+        .resume-item-top-left{
+          width:80%;
+          display:flex;
+          align-items:center;
+          font-size:1.1rem;
+        }
+        .resume-item-top-right{
+          width:20%;
+          display:flex;
+          flex-direction:row;
+          justify-content:space-evenly;
+          align-items:flex-end;
+          font-size:1.7rem;
+        }
+        .resume-item-bottom{
+          margin-top:10px;
+          width:100%;
+          display:flex;
+          color:${styleConstants.color.secondary[1]}
+        }
+        .resume-item-bottom-tech{
+          margin:0 2px;
+          padding:3px 4px;
+          font-size:0.9rem;
+          border-radius:10px;
+          background-color:${styleConstants.color.primary[0]}
+        }
       `}</style>
       <Layout pageTitle={`${title} | Resume`} description={description}>
         <div className="resume-container">
@@ -75,17 +104,95 @@ export default function Contact({ title, description, ...props }){
                 <h4> Projects </h4>
               </div>
               <div className="resume-section-content projects">
+                
                 <div className="resume-item">
                   <div className="resume-item-top">
-                    <h4> CoCode </h4>
-                    <span>&nbsp;-&nbsp; </span>
-                    <p> Code tutoring over a collaborative enviroment </p>
+                    <div className="resume-item-top-left">
+                      <h4> CoCode </h4>
+                      <span>&nbsp;-&nbsp; </span>
+                      <p> Code tutoring revamped </p>
+                    </div>
+                    <div className="resume-item-top-right">
+                        <a href="https://github.com/parsasi/cocode">
+                          <FontAwesomeIcon icon={faGithub} />
+                        </a>
+                        <a href="http://cocode.space">
+                          <FontAwesomeIcon icon={faLink} />
+                        </a>
+                    </div>
+                  </div>
+                  <div className="resume-item-bottom">
+                    <div className="resume-item-bottom-tech"> #ReactJs </div>
+                    <div className="resume-item-bottom-tech"> #NestJs </div>
+                    <div className="resume-item-bottom-tech"> #WebRTC </div>
+                    <div className="resume-item-bottom-tech"> #WebSockets </div>
                   </div>
                 </div>
 
                 <div className="resume-item">
-                  
+                  <div className="resume-item-top">
+                    <div className="resume-item-top-left">
+                      <h4> CodeJar </h4>
+                      <span>&nbsp;-&nbsp; </span>
+                      <p> Live, collaborative coding enviroment </p>
+                    </div>
+                    <div className="resume-item-top-right">
+                        <a href="https://github.com/parsasi/codejar">
+                          <FontAwesomeIcon icon={faGithub} />
+                        </a>
+                        <a href="https://codejar.live">
+                          <FontAwesomeIcon icon={faLink} />
+                        </a>
+                    </div>
+                  </div>
+                  <div className="resume-item-bottom">
+                    <div className="resume-item-bottom-tech"> #ReactJs </div>
+                    <div className="resume-item-bottom-tech"> #NodeJs </div>
+                    <div className="resume-item-bottom-tech"> #Redux </div>
+                    <div className="resume-item-bottom-tech"> #WebSockets </div>
+                  </div>
                 </div>
+
+                <div className="resume-item">
+                  <div className="resume-item-top">
+                    <div className="resume-item-top-left">
+                      <h4> Lustr </h4>
+                      <span>&nbsp;-&nbsp; </span>
+                      <p> Web Based Dating App </p>
+                    </div>
+                    <div className="resume-item-top-right">
+                        <a href="https://github.com/parsasi/lustr">
+                          <FontAwesomeIcon icon={faGithub} />
+                        </a>
+                    </div>
+                  </div>
+                  <div className="resume-item-bottom">
+                    <div className="resume-item-bottom-tech"> #ReactJs </div>
+                    <div className="resume-item-bottom-tech"> #Firebase </div>
+                    <div className="resume-item-bottom-tech"> #NodeJs </div>
+                  </div>
+                </div>
+
+                <div className="resume-item">
+                  <div className="resume-item-top">
+                    <div className="resume-item-top-left">
+                      <h4> Stock Up </h4>
+                      <span>&nbsp;-&nbsp; </span>
+                      <p> Store Management Windows Application </p>
+                    </div>
+                    <div className="resume-item-top-right">
+                        <a href="https://github.com/parsasi/stockup">
+                          <FontAwesomeIcon icon={faGithub} />
+                        </a>
+                    </div>
+                  </div>
+                  <div className="resume-item-bottom">
+                    <div className="resume-item-bottom-tech"> #CSharp </div>
+                    <div className="resume-item-bottom-tech"> #EntityFramework </div>
+                    <div className="resume-item-bottom-tech"> #DotNetCore </div>
+                  </div>
+                </div>
+
               </div>
 
             </div>
